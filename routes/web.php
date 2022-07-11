753c60abe0301f13772ccd213b19238af9963b64
olga_demina <olga.demina1410@gmail.com>
2022-07-11 11:26:17 +0200

    Scaffolding vue.js per frontoffice completato

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
@@ -28,3 +25,7 @@ Route::middleware('auth')
     ->group(function() {
         Route::get('/', 'HomeController@index')->name('home');
     });
+
+Route::get('{any?}', function() {
+    return view('guest.index');
+})->where('any', '.*');
